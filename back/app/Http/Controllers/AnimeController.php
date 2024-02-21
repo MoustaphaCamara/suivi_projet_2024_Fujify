492f68fe4016335f856cf8d6e0762901e4997877
MoustaphaHCSE <moustapha.camara@helloce.fr>
2024-02-21 20:28:01 +0100

    feat(controller): anime routes return api

--- back/app/Http/Controllers/AnimeController.php
+++ back/app/Http/Controllers/AnimeController.php
@@ -3,48 +3,56 @@
 namespace App\Http\Controllers;
 
 use App\Models\Anime;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class AnimeController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): Collection
     {
-        return response()->json(['name' => 'Anime1', 'type' => 'Isekai']);
+        return Anime::all();
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(Request $request): JsonResponse
     {
-        //
+        $anime = Anime::create($request->all());
+        return response()->json($anime, Response::HTTP_CREATED);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(string $id): JsonResponse
     {
-        //
+        $anime = Anime::find($id);
+        return response()->json($anime);
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(Request $request, string $id): JsonResponse
     {
-        //
+        $anime = Anime::find($id);
+        $anime->update($request->all());
+        return response()->json($anime);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(string $id): JsonResponse
     {
-        //
+        $anime = Anime::find($id);
+        $anime->delete();
+        return response()->json(null, 204);
     }
 }
