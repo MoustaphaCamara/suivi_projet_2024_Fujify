9506571b28ddd05f32c197e63447c073c8fc3f19
MoustaphaCamara <cmr.mous@gmail.com>
2024-03-07 13:15:13 +0100

    feat(requests): quick fixes (typing & useless doc), sprintf strings

--- back/app/Http/Requests/ArtistRequest.php
+++ back/app/Http/Requests/ArtistRequest.php
@@ -6,20 +6,11 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class ArtistRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize()
+    public function authorize(): bool
     {
         return true;
     }
-
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules()
+    public function rules(): array
     {
         return [
             'name' => ['required', 'string', 'max:255'],
@@ -29,12 +20,7 @@ class ArtistRequest extends FormRequest
         ];
     }
 
-    /**
-     * Get custom attributes for validator errors.
-     *
-     * @return array
-     */
-    public function attributes()
+    public function attributes(): array
     {
         return [
             'name' => 'Name',
