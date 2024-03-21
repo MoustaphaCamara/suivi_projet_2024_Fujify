1f7147b22c2eac2f3a9e74c38e12039b999cc208
MoustaphaCamara <cmr.mous@gmail.com>
2024-03-21 16:23:48 +0100

    feat(button): custom colors & disable

--- front/src/components/ButtonFJ.jsx
+++ front/src/components/ButtonFJ.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 
 const ButtonFJ = (props) => {
+    const [color, setColor] = useState("");
+    const [isDisabled, setIsDisabled] = useState(false);
 
+    useEffect(() => {
+        if (props.variant === "disabled") {
+            setIsDisabled(true)
+        }
+        switch (props.variant) {
+            case "pink-black":
+                setColor("bg-[#BD5784] text-black");
+                break;
+            case "white-black":
+                setColor("bg-[#D9D9D9] text-black");
+                break;
+            case "white-pink":
+                setColor("bg-[#D9D9D9] text-[#BD5784]");
+                break;
+            case "black-white":
+                setColor("bg-black text-white");
+                break;
+            case "black-pink":
+                setColor("bg-black text-[#BD5784] border-2 border-[#BD5784]");
+                break;
+            case "disabled":
+                setColor("cursor-not-allowed bg-[#A6A6A6] text-white border-2 border-white");
+                break;
+            default:
+                setColor("bg-[#BD5784] text-white ");
+        }
+    }, [color]);
     return (
-        <button className={props}>
-            ButtonFJ
+        <button disabled={isDisabled} className={color}>
+            {props.content}
         </button>
     );
 };
