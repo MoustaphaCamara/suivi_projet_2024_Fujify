990d255442cadfb22ee89e14258f99489c87f60e
MoustaphaCamara <cmr.mous@gmail.com>
2024-03-21 14:23:14 +0100

    chore(packages): add router, axios, tailwind

---
+++ front/src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HomePage = () => {
+    return (
+        <div>
+            <h1 className="text-[#ff2d20]">
+                Hello world!
+            </h1>
+        </div>
+    );
+};
+
+export default HomePage;
